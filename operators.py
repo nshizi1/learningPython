82592b3845333ed27d7198f8a0d9d86252e4e74b
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2024-03-07 22:46:50 +0200

    operators

---
+++ operators.py
@@ -0,0 +1,47 @@
+# 1. Arithmetic
+# 2. Comparison
+# 3. Logical
+# 4. Assignment
+# 5. Membership
+# 6. Identity
+# 7. Bitwise
+
+# arithmetic operators
+
+# + - / * %
+
+a = 5
+b=8
+c = a-b
+print(a-b)
+print(c)
+print(f"In Subtraction,{a}-{b} is {c}")
+print(f"In Addition,{a}+{b} is {a+b}")
+print(f"In Multiplication,{a}*{b} is {a*b}")
+print(f"In Division,{b}/{a} is {b/a}")
+print(f"In Floor division,{b}//{a} is {b//a}")
+print(f"The modulus of {b} to {a} is {b%a}")
+print(f"The exponential of {b} to {a} is {b**a}")
+print(f"The exponential of 3 to 2 is {3**2}")   # means 3 on the exponent of 2 which equals 9. 3 power 2
+
+# Comparison operators
+
+a = 3
+b = 9
+if a == b:
+    print("a is equal to b")
+else:
+     print("a is greater than b")
+
+if a != b:
+    print("a is not equal to b")
+else:
+    print("a is equal b")
+    
+if a < b:
+    print("a is smaller than b")
+else:
+    print("a is greater than b")
+    
+# other comparison operators are not implemented like: >, <=, >=
+
