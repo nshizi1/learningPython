56b71bb9d41c7e24e2a6fa1020943a390c0041c2
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2024-03-07 23:14:11 +0200

    control flow conditions

---
+++ controlFlow.py
@@ -0,0 +1,27 @@
+# Conditional statement: if,  if-else, if-elif-else, and nested if
+
+age = 23
+if age < 18:
+    print("You are not old enough to vote.")
+elif age > 50:
+    print("You are too old to vote.")
+else:
+    print("You are allowed to vote")
+    
+# The above can have if, if-else and if-elif-else statements
+# nested if-else statements
+
+a = 0
+b = 3
+c = 1
+if a > b:
+    if a> c:
+        print("a is greatest")
+    else:
+        print("c is greatest")
+elif b > c:
+        print("B is greatest")
+else:
+    print("C is greatest")
+    
+    
