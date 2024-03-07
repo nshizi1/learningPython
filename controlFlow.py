d52f8baa6856ebe7dd44654ff954620ed758ecf6
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2024-03-07 23:45:53 +0200

    switchCase like

--- controlFlow.py
+++ controlFlow.py
@@ -23,5 +23,28 @@ elif b > c:
         print("B is greatest")
 else:
     print("C is greatest")
+ 
+# if like switch case
+
+def week(number):
+    if number == 1:
+        print("Monday")
+    elif number == 2:
+        print("Tuesday")
+    elif number == 3:
+        print("Wednesday")
+    elif number == 4:
+        print("Thursday")
+    elif number == 5:
+        print("Friday")
+    elif number == 6:
+        print("Saturday")
+    elif number == 7:
+        print("Sunday")
+    else:
+        print("Invalid day")
+
     
-    
+day  = int(input("Enter day number you want to display: "))
+
+week(day)
