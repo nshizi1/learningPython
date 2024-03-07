2dbfe7f868021d58b8e1605c0d1d53742ff47fda
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2024-03-07 22:28:38 +0200

    datatypes

--- variables.py
+++ variables.py
@@ -1,23 +1,30 @@
-number = 5
+number = 5  #integer
 print(number)
-floatPoint = 5.5
+floatPoint = 5.5    #float point
 print(floatPoint)
-names = "Wilson Nshizirungu"
+names = "Wilson Nshizirungu"    #string
 print(names)
 # Concatenating Strings
 fName = "Wilson"
 lName = "Nshizirungu"
 fullName = fName+" "+lName
 print(fullName)
-fruits = ["mango", "banana", "orange", "apple"]
+fruits = ["mango", "banana", "orange", "apple"]     #list
 for fruit in fruits:
     print(fruit)
-foods = ("meat", "rice", "beans", "maize")
+foods = ("meat", "rice", "beans", "maize")      #tuple
 for food in foods:
     print(food)
 person = {"name": "Wilson",
           "age": 25,
-          "city": "Kigali"}
+          "city": "Kigali"}     #dictionary
 for key, value in person.items():
     print(key,": ",value ) #this method
     print(f"{key}: {value}") #or this one
+
+null = None #None datatype, same as the null datatype
+print(null)
+
+numbers = {1,2,3}   #set datatype
+for number in numbers:
+    print(number)
