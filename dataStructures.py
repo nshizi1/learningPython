8cf2360272f8d092e2d94829d4cb57e901a0aeb2
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2024-03-09 02:17:08 +0200

    data structures

--- dataStructures.py
+++ dataStructures.py
@@ -35,5 +35,65 @@ print(myTuple[3])   # 4
 
 # mySet = set(myTuple)    #will have values of the tuple
 # mySet = set([1, 4, 1, 5, 9])  #this can also work
-mySet = {1, 4, 1, 5, 9}
+mySet = {1, 4, 1, 5, 9, 1, 3}
 print("set: ", mySet)
+mySet.add(3) 
+# Note that a value can be only added once
+print("set: ", mySet)   #output is :{1, 3, 4, 5, 9}  #One is used once
+mySet.remove(1) 
+print(mySet)    #All ones are removed
+
+
+# Dictionaries: set of key-value pairs
+
+# Syntax: dictionaryName = {key1: value1, key2: value2, ...}
+
+myDict = {"name": "Wilson", "age": 23, "address": "Kigali"}
+print(myDict["name"])   #Outputs WIlson
+print(myDict)   #prints all the data in a dictionary    
+# Adding new value to dictionary
+myDict["Hobby"] = "Cooking"
+print(myDict["Hobby"])      #Outputs Cooking
+# updating value of key
+myDict["age"] = 18
+print(myDict["age"])    #now outputs 18
+
+# removing keys
+del myDict["address"]
+# print(myDict["address"]) #Will generate error message coz key is deleted
+print(myDict)
+# Dictionary methods
+print(myDict.keys())    #outputs keys only: name, age, hobby
+print(myDict.values()) #outputs values only: Wilson, 18, Cooking
+print(myDict.items())   #outputs keys & values pairs as tuple: ([('name', 'Wilson'), ('age', 18), ('Hobby', 'Cooking')])
+if "name" in myDict:    #checking existence of keys
+    print(f"Key exists with value {myDict['name']}")
+else:
+    print("Key does not exist")
+
+# nested dictionary
+nestDict = {"names": {
+    "first name": "Wilson",
+    "last name": "Nshizirungu"},
+            "age": 23,
+            "address": "Kigali"}
+# display first and last names
+print(nestDict["names"])
+# first name only
+print(nestDict["names"]["first name"])
+if "first name" in nestDict:
+    print(f"Key exists with value {nestDict['names']['first name']}")
+else:
+    print("Key does not exist")
+# the above will fail coz key : "first name" is in "name", do the following instead
+    
+if "first name" in nestDict["names"]:
+    print(f"Key exists with value {nestDict['names']['first name']}")
+else:
+    print("Key does not exist")
+
+# checking if names exists in the dictionary    
+if "names" in nestDict:
+    print(f"Key exists with value {nestDict['names']}")
+else:
+    print("Key does not exist")
